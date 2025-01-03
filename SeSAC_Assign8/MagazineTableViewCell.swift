80a446a95bfdf6aa30bb7c43a6dc885b12067b82
imo2k <dnrwk99@gmail.com>
2025-01-03 21:14:10 +0900

    MagazineInfo 화면 구성 + Cell내 이미지 킹피셔 적용

---
+++ SeSAC_Assign8/MagazineTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MagazineTableViewCell.swift
+//  SeSAC_Assign8
+//
+//  Created by GO on 1/3/25.
+//
+
+import UIKit
+
+class MagazineTableViewCell: UITableViewCell {
+    
+    @IBOutlet var magazineImage: UIImageView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var subtitleLabel: UILabel!
+    
+    @IBOutlet var dateLabel: UILabel!
+}
+
