8a5ae5a1d457295d8733a82c42ead17280db01d4
imo2k <dnrwk99@gmail.com>
2025-01-03 19:33:18 +0900

    Initial Commit

---
+++ SeSAC_Assign8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Assign8
+//
+//  Created by GO on 1/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
