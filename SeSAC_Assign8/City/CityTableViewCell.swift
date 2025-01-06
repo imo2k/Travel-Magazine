5cfe54ed3fc0c98b4dc1abb3b15b3362499a25fe
imo2k <dnrwk99@gmail.com>
2025-01-07 02:59:39 +0900

    커밋 실수로 인한 재커밋
    Segmented Controller에 domestic_travel의 Bool 값에 따라 다른 Cell 구성되게 적용
    (selectedSegmentIndex == 0 to 1, 2는 이상 X, selectedSegmentIndex == 1 to 2, 2 to 1 화면 전환 시 Cell 안보이는 이슈)

--- SeSAC_Assign8/City/CityTableViewCell.swift
+++ SeSAC_Assign8/City/CityTableViewCell.swift
@@ -6,18 +6,55 @@
 //
 
 import UIKit
+import Kingfisher
 
 class CityTableViewCell: UITableViewCell {
 
+    static let identifier = "CityTableViewCell"
+    
+    @IBOutlet var cellImageView: UIImageView!
+    
+    @IBOutlet var bottomBackgroundView: UIView!
+    
+    @IBOutlet var cityNameLabel: UILabel!
+    
+    @IBOutlet var cityListLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        configure()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        cellImageView.image = UIImage(systemName: "xmark")
     }
+    private func configure() {
+        cellImageView.layer.cornerRadius = 15
+        cellImageView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMaxYCorner)
+        bottomBackgroundView.backgroundColor = .black
+        bottomBackgroundView.layer.opacity = 0.5
+        bottomBackgroundView.layer.cornerRadius = 15
+        bottomBackgroundView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMaxXMaxYCorner)
+        cityNameLabel.textColor = .white
+        cityNameLabel.font = .boldSystemFont(ofSize: 24)
+        cityListLabel.textColor = .white
+        cityListLabel.font = .systemFont(ofSize: 14)
+        cityNameLabel.textAlignment = .right
+        cityListLabel.textAlignment = .left
+        
+    }
+    
+    func configureData(row: City) {
+        let image = row.city_image
+        
+        let url = URL(string: image)
+        cellImageView.kf.setImage(with: url)
+        
+        cityNameLabel.text = "\(row.city_name) | \(row.city_english_name)"
+        cityListLabel.text = row.city_explain
+    }
+    
+    
     
 }
--- SeSAC_Assign8/City/CityTableViewController.swift
+++ SeSAC_Assign8/City/CityTableViewController.swift
@@ -6,84 +6,113 @@
 //
 
 import UIKit
+import Kingfisher
+
+struct City {
+    let city_name: String
+    let city_english_name: String
+    let city_explain: String
+    let city_image: String
+    let domestic_travel: Bool
+}
+
+struct CityInfo {
+    let city: [City] = [
+        City(city_name: "방콕", city_english_name: "Bangkok", city_explain: "방콕, 파타야, 후아힌, 코사멧, 코사무이", city_image: "https://images.unsplash.com/photo-1716872491897-078d9b89be49?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: false),
+        City(city_name: "오사카", city_english_name: "Osaka", city_explain: "오사카, 교토, 고베, 나라", city_image: "https://images.unsplash.com/photo-1716881768763-4088391a445e?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: false),
+        City(city_name: "다낭", city_english_name: "Danang", city_explain: "다낭, 호이안, 후에", city_image: "https://images.unsplash.com/photo-1716619240251-54a22779ed8a?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: false),
+        City(city_name: "제주", city_english_name: "Jeju", city_explain: "제주, 섭지코지, 성산일출봉", city_image: "https://images.unsplash.com/photo-1716565679084-2c3dbececc5e?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: true),
+        City(city_name: "부산", city_english_name: "Busan", city_explain: "부산, 해운대, 마린시티", city_image: "https://images.unsplash.com/photo-1716619222059-62e8670293e6?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: true),
+        City(city_name: "파리", city_english_name: "Paris", city_explain: "파리, 베르사유, 몽생미셀, 스트라스부르", city_image: "https://images.unsplash.com/photo-1715638427009-8b0fe7096838?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: false),
+        City(city_name: "시드니", city_english_name: "Sydney", city_explain: "시드니, 블루마운틴, 울릉공, 뉴캐슬", city_image: "https://images.unsplash.com/photo-1716117273853-75a1989029f2?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: false),
+        City(city_name: "전주", city_english_name: "Jeonju", city_explain: "전주, 한옥 마을, 오목대, 경기전", city_image: "https://images.unsplash.com/photo-1715646527352-3e9a4e406952?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: true),
+        City(city_name: "밀라노", city_english_name: "Milano", city_explain: "밀라노, 꼬모, 베로나, 베르가모, 시르미오네", city_image: "https://plus.unsplash.com/premium_photo-1715616257496-5e14778bbc0c?q=80&w=3415&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: false),
+        City(city_name: "리스본", city_english_name: "Lisbon", city_explain: "리스본, 신트라, 카스카이스, 오비두스", city_image: "https://images.unsplash.com/photo-1715559067654-d485ab2618aa?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: false),
+        City(city_name: "서울", city_english_name: "Seoul", city_explain: "서울, 남산타워, 롯데타워, 경복궁", city_image: "https://images.unsplash.com/photo-1715880005923-0013a6820a72?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: true),
+        City(city_name: "두바이", city_english_name: "Dubai", city_explain: "두바이, 아부다비, 알 아인", city_image: "https://images.unsplash.com/photo-1715073145727-393bbded41d9?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: false),
+        City(city_name: "홍콩", city_english_name: "Hongkong", city_explain: "홍콩, 마카오", city_image: "https://images.unsplash.com/photo-1715547748806-dab4f4dfbc85?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: false),
+        City(city_name: "토론토", city_english_name: "Toronto", city_explain: "토론토, 나이아가라, 킹스턴, 블루마운틴", city_image: "https://images.unsplash.com/photo-1715645943531-a57960d41818?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: false),
+        City(city_name: "대전", city_english_name: "Daejeon", city_explain: "대전, 성심당", city_image: "https://plus.unsplash.com/premium_photo-1695084221958-079096c96e05?q=80&w=3461&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", domestic_travel: true),
+    ]
+}
 
 class CityTableViewController: UITableViewController {
-
+    
+    // 😭 domestic_travel에 접근하기 위해서 개념을 살짝 정리를 해봤습니다 (혹시 틀렸다면 지도편달 부탁드립니다..ㅠㅠ)
+    
+    // CityInfo() 인스턴스에 city 프로퍼티에 접근해서 City 구조체의 배열 가져옴
+    // + struct는 값 타입 -> 구조체 City에서 선언된 (city_name,city_english_name, city_explain, city_image,domestic_travel)인스턴스 들의 값을 복사해옴 (각 프로퍼티(상수)들은 저장 인스턴스 프로퍼티임
+    /*
+     구조체 CityInfo 내부에 선언된 city 프로퍼티(저장, 인스턴스 프로퍼티)는 구조체 City의 배열을 타입으로 가짐
+     
+     밑에서 선언된 city는 구조체 CityInfo의 새로운 인스턴스를 생성한 것이고
+     구조체 CityInfo에서 선언된 저장, 인스턴스 프로퍼티인 city 프로퍼티에 접근함
+     */
+    
+    // + 네이밍의 중요함 배웠습니다.. ☠️구분 잘되게 네이밍하기☠️
+    var selectedCity = CityInfo().city // let city: [City] = CityInfo().city의 축약형
+     
+    @IBOutlet var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        titleLabel.text = "인기 도시"
+        titleLabel.font = .boldSystemFont(ofSize: 20)
+        titleLabel.textAlignment = .center
+        
+        // 레지스터
+        let nib = UINib(nibName: "CityTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "CityTableViewCell")
+        
     }
-
+    
+    @IBAction func segmentedControllTapped(_ sender: UISegmentedControl) {
+        let segmentIndex = sender.selectedSegmentIndex
+        
+        
+        // 세그먼트 컨트롤에서 2개만 화면이 잘나오는 이슈 발생
+        // -> 모두 탭을 찍고 다시 다른 탭을 선택하면 나오긴 하지만 국내 - 해외 (안나옴), 해외 - 국내 (안나옴)
+        if segmentIndex == 1 { // 국내
+            print("국내")
+            selectedCity = selectedCity.filter { city in city.domestic_travel == true }
+            tableView.reloadData()
+        } else if segmentIndex == 2 { // 해외
+            print("해외")
+            selectedCity = selectedCity.filter { city in city.domestic_travel == false }
+            tableView.reloadData()
+        } else { // 전체
+            print("전체")
+            
+            selectedCity = CityInfo().city
+            tableView.reloadData()
+        }
+        
+    }
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
+    // 셀 개수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return selectedCity.count
     }
-
-    /*
+    
+    // 셀 데이터 + 디자인(CityTableViewCell)
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: CityTableViewCell.identifier) as! CityTableViewCell
+        
+        let row = selectedCity[indexPath.row]
+        
+        cell.configureData(row: row)
+        
+        
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    // 셀 높이
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
